8f273a3aa7ad128f17022c9cf24d68f8f049149c
logika <logikalogeswaran@gamil.com>
2026-04-28 12:49:57 +0530

    add to app.jsx

---
+++ src/components/footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return(
+        <>
+        <h1>This is footer</h1>
+        </>
+    )
+}
+export default Footer
---
+++ src/components/header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+    return(
+        <>
+        <h1>header components</h1>
+        </>
+    )
+}
+export default Header
